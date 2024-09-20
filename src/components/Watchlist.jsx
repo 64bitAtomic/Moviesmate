8988a675185b0b5fe9febe7705aeb06e7b2f225d
RecurrciveItu <149315193+RecurrciveItu@users.noreply.github.com>
2024-09-20 18:54:15 +0530

    Watchlist

--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -2,9 +2,52 @@ import React from "react";
 
 const Watchlist = () => {
   return (
-    <div>
-      <h1>This is watchlist</h1>
-    </div>
+    <>
+      <div className="flex justify-center flex-wrap m-4">
+        <div className="flex justify-center items-center h-[3rem] w-[9rem] bg-blue-400 rounded-xl text-white font-bold  mx-4">
+          Action
+        </div>
+        <div className="flex justify-center items-center h-[3rem] w-[9rem] bg-gray-400/50 rounded-xl text-white font-bold ">
+          Thriller
+        </div>
+      </div>
+
+      <div className="flex justify-center my-4">
+        <input
+          type="text"
+          placeholder="Search Movies"
+          className="h-[3rem] w-[18rem] bg-gray-200 outline-none px-4"
+        />
+      </div>
+      <div className="overflow-hiiden rounded-lg border border-gray-200	 m-8">
+        <table className="w-full text-gray-500 text-center">
+          <thead className="border-b-2">
+            <tr>
+              <th>Name</th>
+              <th>Rating</th>
+              <th>Popularity</th>
+              <th>Genre</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border-b-2">
+              <td className="flex items-center px-6 py-4">
+                <img
+                  className="h-[6rem] w-[10rem]"
+                  src={`https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/66a4263d01a185d5ea22eeec_6408f6e7b5811271dc883aa8_batman-min.png`}
+                  alt=""
+                />
+                <div className="mx-10">The Batman</div>
+              </td>
+              <td>8.9</td>
+              <td>9</td>
+              <td>Action,Thriller</td>
+              <td className="text-red-800">Delete</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
